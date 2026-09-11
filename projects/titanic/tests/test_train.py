1288d03e6c34d06247d4f83ab080eaedecbd8b44
Febriyansyah Research <febriyansyahresearch@users.noreply.github.com>
2026-09-11 15:38:56 +0700

    chore: restructure per collection (COMPUTER-VISION), remove projects and Dockerfile

--- projects/titanic/tests/test_train.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from projects.titanic.src.train import train
-
-
-def test_train_returns_metrics():
-    metrics = train(n_estimators=10)
-    assert "accuracy" in metrics
-    assert "report" in metrics
-    assert metrics["accuracy"] > 0
